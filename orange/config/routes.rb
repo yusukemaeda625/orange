2f1df592efed8ba0b20876f42c17e836f36b52c6
maedayusuke <yousecome635@gmail.com>
2018-12-13 01:17:25 +0900

    #25 登録した属性の削除機能

--- orange/config/routes.rb
+++ orange/config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
 	
 	get "/admin", to:"pages#admin"
 	
+	get "delete_attributes/:id", to:"pages#delete_attributes"
+	
 	post "create_attributes", to:"pages#create_attributes"
 
 	post "add_attributes", to:"pages#add_attributes"
